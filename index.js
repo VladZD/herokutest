6a1d3e841b0eef60f21483b4540eeaa0e0a4858d
Vlad Drozd <vladbbird@gmail.com>
2017-09-18 15:27:56 +0300

    Add secret route that returns config variable specified in heroku settings

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('.'));
+app.get('/secret', function(req, res) {
+  res.send(process.env.CODE || 'There\'s no secret code');
+});
 app.listen(process.env.PORT || 3000);
